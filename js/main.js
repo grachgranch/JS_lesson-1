af4ddd2efc5c77db331ecc13171a099db42df2dd
grachgranch <63392201+grachgranch@users.noreply.github.com>
2020-04-29 17:39:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+let student_1 = {
+  name: "Dan",
+  grade: 2,
+  age: null,
+  expelling: true,
+};
+let student_2 = {
+  name: "Maya",
+  grade: 4,
+  age: null,
+  expelling: false,
+};
+let student_3 = {
+  name: "Sarah",
+  grade: 4,
+  age: null,
+  expelling: false,
+};
+let student_4 = {
+  name: "Alef",
+  grade: 3,
+  age: null,
+  expelling: false,
+};
+let student_5 = {
+  name: "Deborah",
+  class: 1,
+  age: null,
+  expelling: false,
+};
+
+let StudentBase = [student_1, student_2, student_3, student_4, student_5];
+
+console.log(StudentBase);
